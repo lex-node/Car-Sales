4833504539f39ed6eab3302cbf753030a6f7f784
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-27 00:04:40 -0400

    configures AdditionalFeatures for redux

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import {connect} from 'react-redux';
 
-const AdditionalFeatures = props => {
-  return (
-    <div className="content">
-      <h4>Additional Features</h4>
-      {props.store.length ? (
-        <ol type="1">
-          {props.store.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
-          ))}
-        </ol>
-      ) : (
-        <p>Nice looking car!</p>
-      )}
-    </div>
-  );
+class AdditionalFeatures extends React.Component {
+    state = {
+        store: [
+            {id: 1, name: 'V-6 engine', price: 1500},
+            {id: 2, name: 'Racing detail package', price: 1500},
+            {id: 3, name: 'Premium sound system', price: 500},
+            {id: 4, name: 'Rear spoiler', price: 250}
+        ]
+    }
+
+    render() {
+        return (
+            <div className="content">
+                <h4>Additional Features</h4>
+                {this.state.store.length ? (
+                    <ol type="1">
+                        {this.state.store.map(item => (
+                            <AdditionalFeature key={item.id} feature={item}/>
+                        ))}
+                    </ol>
+                ) : (
+                    <p>Nice looking car!</p>
+                )}
+            </div>
+        );
+    }
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+    return {
+        store: state.store
+    }
+}
+
+
+export default connect(
+    mapStateToProps,
+    {}
+)(AdditionalFeature);
