98560c0abd1949c279a242e207738542ee4e158d
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-26 21:40:48 -0400

    configure index.js for redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {Provider} from 'react-redux';
+import {createStore} from 'react-redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(/*SOME REDUCER*/)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider><App/></Provider>, rootElement);
