05560783aec1eb51928bbcf08195ec099dd996ae
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-28 22:10:46 -0400

    modifies ADD_FEATURE case of reducer so that an additional feature is only added if it has not already been added

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -18,15 +18,19 @@ const initialState = {
 function reducer(state = initialState, action) {
     switch (action.type) {
         case 'ADD_FEATURE':
-            let newAdditionalPrice = state.additionalPrice + action.payload.price;
-            let newCar = {...state.car, features: [...state.car.features, action.payload]}
-            return {...state, additionalPrice: newAdditionalPrice, car: newCar};
+            if (!(state.car.features.includes(action.payload))) {
+                let newAdditionalPrice = state.additionalPrice + action.payload.price;
+                let newCar = {...state.car, features: [...state.car.features, action.payload]};
+                return {...state, additionalPrice: newAdditionalPrice, car: newCar};
+            } else {
+                return {...state}
+            }
         case 'REMOVE_FEATURE':
             let newFeatures = state.car.features.filter(feature => {
                 return !(feature.id === action.payload.id)
             });
             let newRemovedAdditionalPrice = state.additionalPrice - action.payload.price;
-            let newRemovedFeatureCar = {...state.car, features: newFeatures}
+            let newRemovedFeatureCar = {...state.car, features: newFeatures};
             return {...state, additionalPrice: newRemovedAdditionalPrice, car: newRemovedFeatureCar};
         default:
             return state;
